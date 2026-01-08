b7f04755717dbfd923471c6e1e042759b6cdd5bb
Morpheus <wangmykj2001@gmail.com>
2026-01-08 09:02:05 +0900

    Add data validation in `_load_json()`
    Also stored JSON key names as DQTJSON attributes
    
    Corrected argument for `rename()` to use full path instead of filename.

--- dqt_json.py
+++ dqt_json.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -19,33 +20,98 @@ class DQTJSON:
         self.json_path = self.parent_dir / self.json_name
         self.json_path_pre5 = self.parent_dir / self.json_name_pre5
 
+        self.rating_key_name = 'rating'
+        self.mem_entry_key_name = 'memory'
+
+        self._touch()
         self.saved_ratings = self._load_json()
 
-    def touch(self) -> None:
-        """Check if JSON file exists."""
+    def _touch(self) -> None:
+        """Check if JSON file exists, create if not."""
         if not self.json_path.exists():
             if self.json_path_pre5.exists():
-                self.json_path_pre5.rename(self.json_name)
+                self.json_path_pre5.rename(self.json_path)
             else:
                 self.json_path.touch()
 
     def update(self):
         """Dump updated `saved_ratings` dict to JSON file."""
-        if not self.json_path.exists():
-            self.json_path.touch()
-
         with open(self.json_path, 'w') as json_file:
             json.dump(self.saved_ratings, json_file, indent=4)
 
-    def _load_json(self) -> dict:
-        """Load JSON file contents and return dict."""
-        if not self.json_path.exists():
-            self.json_path.touch()
+    def _load_json(self) -> dict[str, dict[str, float | str]]:
+        """Load JSON file contents and return dict.
 
-        if not self.json_path.read_text():
+        Validate dict before returning. If the dict is missing a rating item,
+        raise a KeyError. If a memory entry item is missing, add default memory
+        entry. If date keys are not in increasing consecutive order, raise a
+        ValueError.
+        """
+        if not self.json_path.exists():
             return {}
+        if not self.json_path.read_text().strip():
+            return {}
+
         with open(self.json_path, 'r') as file:
             contents = json.load(file)
-        return {
-            k: float(v) for k, v in contents.items()
-        }
+
+        updated = False
+        prev_date = None
+        validated: dict[str, dict[str, float | str]] = {}
+
+        for date, value in contents.items():
+
+            # Validate date keys
+            if prev_date is not None:
+                prev_d = datetime.strptime(prev_date, self.date_format)
+                d = datetime.strptime(date, self.date_format)
+                diff = (d - prev_d).days
+                if diff < 0:
+                    raise ValueError(
+                        f"Date {date} is older than previous date {prev_date}"
+                    )
+                if diff == 0:
+                    raise ValueError(
+                        f"Date {prev_date} is repeated"
+                    )
+
+            prev_date = date
+
+            # ---------- pre-DQT-5 format ----------
+            # { "YYYY-MM-DD": rating }
+            if isinstance(value, (int, float)):
+                validated[date] = {
+                    self.rating_key_name: float(value),
+                    self.mem_entry_key_name: ""  # Default to empty mem-entry
+                }
+                updated = True
+                continue
+
+            # ---------- ≥ DQT-5 format ----------
+            # { "YYYY-MM-DD": { "rating": rating, "memory": memory entry } }
+            if isinstance(value, dict):
+                if self.rating_key_name not in value:
+                    raise KeyError(f"Missing rating for date {date}")
+
+                rating = float(value[self.rating_key_name])
+                memory = value.get(self.mem_entry_key_name, '')
+
+                # Auto-fix missing memory entry
+                if self.mem_entry_key_name not in value:
+                    updated = True
+
+                validated[date] = {
+                    self.rating_key_name: rating,
+                    self.mem_entry_key_name: memory
+                }
+                continue
+
+            # ---------- invalid format ----------
+            raise ValueError(f"Invalid log format for date {date}")
+
+        # Sync JSON if any fixes were applied
+        if updated:
+            with open(self.json_path, 'w') as file:
+                json.dump(validated, file, indent=4)
+
+        return validated
