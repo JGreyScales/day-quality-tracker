cd07109ae72834a00aa37770830a733e76697550
Morpheus <wangmykj2001@gmail.com>
2026-01-14 10:42:51 +0900

    Polish `run_me.py`

--- run_me.py
+++ run_me.py
@@ -7,18 +7,20 @@ if __name__ == '__main__':
     dqt = DayQualityTracker()
 
     # Customizations go here ↴
-
+    dqt.rating_inp_dp = 2
     # ------------------------ #
 
     try:
         dqt.run()
     except KeyboardInterrupt:
-        print("\n\nUser interrupted the program.")
+        print("\n\n*⎋* —————————————————————————————— *⎋*")
+        print("\nUser interrupted the program.")
         print("\nSaving changes...")
         dqt.json.update()
         print("Success!")
         sys.exit()
     except Exception:
+        print("\n*!* —————————————————————————————— *!*")
         print("\n❌ \033[1m\033[31mError!\033[0m")
         print(traceback.format_exc())
         sys.exit(1)
