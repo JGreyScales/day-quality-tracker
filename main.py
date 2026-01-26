2fe9e59ae56d95b372e43638dcf6e1a341b6815a
Morpheus <wangmykj2001@gmail.com>
2026-01-26 23:16:38 +0900

    Improve exception handling in main.py

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 """Run this module to start Day Quality Tracker."""
 
-import sys
-import traceback
-
-from dqt.tracker import Tracker
-from dqt.styletext import StyleText as Txt
-
 if __name__ == '__main__':
-    dqt = Tracker()
-
-    # Custom configurations go here ↴
-    dqt.configure(
+    try:
+        import sys
+        import traceback
+        
+        from dqt.styletext import StyleText as Txt
+        from dqt.tracker import Tracker
+    except ModuleNotFoundError as e:
+        print("\n*!* —————————————————————————————— *!*")
+        print(Txt("\n❌Error!").bold().red())
+        print(e)
+        print(f"Ensure that module '{e.name}' exists in the current "
+              f"working directory.")
+        sys.exit(1)
     
-    )
-    dqt.json.configure(
+    try:
+        dqt = Tracker()
     
-    )
-    dqt.graph.configure(
-    
-    )
-
+        # Custom configurations go here ↴
+        dqt.configure(
+        
+        )
+        dqt.json.configure(
+        
+        )
+        dqt.graph.configure(
+        
+        )
+    except ValueError as e:
+        print("\n*!* —————————————————————————————— *!*")
+        print(Txt("\n❌Error!").bold().red())
+        print(e)
+        print("Ensure that you have passed valid configuration keys into the "
+              "`configure()` methods in main.py.")
+        sys.exit(1)
+    except Exception:
+        print("\n*!* —————————————————————————————— *!*")
+        print(Txt("\n❌Error!").bold().red())
+        print(traceback.format_exc())
+        sys.exit(1)
+        
     try:
         dqt.run()
+        
     except KeyboardInterrupt:
         print("\n\n*⎋* —————————————————————————————— *⎋*")
         print("\nUser interrupted the program.")
