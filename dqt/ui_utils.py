f353595cd75f449ff154367199713513d70e7ea1
Morpheus <wangmykj2001@gmail.com>
2026-01-28 12:27:06 +0900

    Make pause optional in `ui_utils.err()`

--- dqt/ui_utils.py
+++ dqt/ui_utils.py
@@ -4,7 +4,7 @@ from time import sleep
 from dqt.styletext import StyleText as Txt
 
 
-def err(message: str, *desc: str) -> None:
+def err(message: str, *desc: str, pause: bool = True) -> None:
     """Print formatted error message."""
     print(
         Txt("\nError: ").bold().red()
@@ -12,7 +12,8 @@ def err(message: str, *desc: str) -> None:
     )
     for d in desc:
         print(d)
-    sleep(1)
+    if pause:
+        sleep(1)
 
 
 def notify_log_saved(text: str = "Log saved!") -> None:
