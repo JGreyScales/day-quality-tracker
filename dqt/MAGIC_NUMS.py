f5257ea063d9fe8aa3065c8e7b9058523cd7b9f0
GreyScales <77678089+JGreyScales@users.noreply.github.com>
2026-02-24 14:37:26 -0500

    Update MAGIC_NUMS.py

--- dqt/MAGIC_NUMS.py
+++ dqt/MAGIC_NUMS.py
@@ -5,4 +5,4 @@ class MAGIC_NUMS:
     Used for storing constants that dont make sense to exist in the settings file or
     must exist in the context of a python class
     """
-    SETTINGS_FILE: Final[str] = "settings.py"
+    SETTINGS_FILE: Final[str] = "settings.json"
